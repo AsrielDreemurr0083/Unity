25e29077aa0bc639a578cacafbce506d76db991c
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-09-20 15:39:46 +0900

    (2024.9.20) 타이틀 화면 제작

--- Run Game/Assets/Scripts/Managers/CoinManager.cs
+++ Run Game/Assets/Scripts/Managers/CoinManager.cs
@@ -5,24 +5,75 @@ using UnityEngine.SceneManagement;
 
 public class CoinManager : MonoBehaviour
 {
-    public GameObject prefab;
+    [SerializeField] GameObject prefab;
+    [SerializeField] List<GameObject> coins;
+
+    [SerializeField] float positionX = 4.0f;
+
     [SerializeField] int createCount = 16;    
     [SerializeField] float offsetZ = 2.5f;
-    // Start is called before the first frame update
+
+
     void Awake()
     {
+        coins.Capacity = 20;
         Create();
+
     }
+
+    
 
     private void Create()
     {
         for(int i = 0; i < createCount; i++) 
         {
+            GameObject clone = Instantiate(prefab);
+
+            clone.transform.SetParent(gameObject.transform);
+
+            clone.transform.localPosition = new Vector3(0, 0, offsetZ * i);
+
+            int index = clone.name.IndexOf("(Clone)");
+
+            if(index > 0)
+            {
+                clone.name = clone.name.Substring(0, index);
+            }
+
+            clone.SetActive(false);
+
+            coins.Add(clone);
+        }
+    }
+
+    /*private void InitializePosition()
+    {
+        for (int i = 0; i < createCount; i++)
+        {
             prefab = Instantiate(prefab);
             prefab.transform.SetParent(gameObject.transform);
-            prefab.transform.localPosition = new Vector3(0,0,offsetZ * i);           
+
+            // x축 위치를 -4, 0, 4 중에서 랜덤 선택
+            float randomX = Random.Range(0f, 4f);
+            if (randomX < 1f)
+            {
+                randomX = -4f;
+            }
+            else if (randomX < 2f)
+            {
+                randomX = 0f;
+            }
+            else
+            {
+                randomX = 4f;
+            }
+
+            prefab.transform.localPosition = new Vector3(randomX, 0, offsetZ * i);
         }
+    }*/
 
-
+    public void InitializePosition()
+    {
+        transform.localPosition = new Vector3(positionX * Random.Range(-1, 2), 0, 0);
     }
 }
