ea95f26bc6712da1197f1d23b2c9a72283d74e39
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-09-26 15:46:43 +0900

    (2024.9.26) 게임 상태 구조 설계

--- Run Game/Assets/Scripts/Coin.cs
+++ Run Game/Assets/Scripts/Coin.cs
@@ -2,16 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour, IColliderable
+public class Coin : State, IColliderable
 {
     [SerializeField] float speed;
     [SerializeField] GameObject rotationObject;
 
-    [SerializeField] ParticleSystem particleSystem;
-
     public void Activate()
     {
-        particleSystem.Play();
 
         gameObject.GetComponent<BoxCollider>().enabled = false;
         gameObject.GetComponent<MeshRenderer>().enabled = false;
@@ -21,15 +18,23 @@ public class Coin : MonoBehaviour, IColliderable
 
     private void OnEnable()
     {
+        base.OnEnable();
+
         rotationObject = GameObject.Find("Rotation GameObject");
 
         speed = rotationObject.GetComponent<RotationObject>().Speed;
 
         transform.localRotation = rotationObject.transform.localRotation;
+
+        
     }
 
     void Update()
     {
+        if(state == false)
+        {
+            return;
+        }
         transform.Rotate(0, speed * Time.deltaTime, 0);
     }
 }
---
+++ Run Game/Assets/Scripts/State.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class State : MonoBehaviour
+{
+    [SerializeField] protected bool state = true;
+
+    protected void OnEnable()
+    {
+        EventManager.Subscribe(EventType.START, OnExcute);
+        EventManager.Subscribe(EventType.STOP, OnStop);
+    }
+
+    protected void OnExcute()
+    {
+        state = true;
+    }
+
+    protected void OnStop()
+    {
+        state = false;
+    }
+
+    protected void OnDisable()
+    {
+        EventManager.UnSubscribe(EventType.START, OnExcute);
+        EventManager.UnSubscribe(EventType.STOP, OnStop);
+    }
+
+}
